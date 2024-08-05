feb5a4b67ddd23486ce689dabf574e40da3a57af
Cesar Augusto <cesardeazevedo@outlook.com>
2024-08-05 22:38:49 +0200

    Use Throttler on useSearch

--- examples/react/src/hooks/useSearch.ts
+++ examples/react/src/hooks/useSearch.ts
@@ -1,21 +1,23 @@
 import { useEffect, useState } from 'react'
-import { pool } from '../nostr'
+import { useThrottle } from "@uidotdev/usehooks";
 import type { NostrEvent } from 'nostr-tools'
+import { pool } from '../nostr'
 
 export function useSearch(search: string) {
   const [items, setItems] = useState<NostrEvent[]>([])
+  const value = useThrottle(search, 500);
 
   useEffect(() => {
-    if (search) {
+    if (value) {
       setItems([])
-      const sub = pool.subscribeMany(['wss://relay.nostr.band'], [{ kinds: [0], search }], {
+      const sub = pool.subscribeMany(['wss://relay.nostr.band'], [{ kinds: [0], search: value }], {
         onevent: (event: NostrEvent) => {
           setItems((prev) => [...prev, event])
         },
       })
       return () => sub.close()
     }
-  }, [search])
+  }, [value])
 
   return items
 }
