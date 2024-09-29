6deef11753abb110c7c5e371cf91a31ec9110663
Cesar Augusto <cesardeazevedo@outlook.com>
2024-09-29 18:59:45 +0200

    Fix bundle size issues

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@
 import { defineConfig } from 'vite'
 import path from 'path'
 import dts from 'vite-plugin-dts'
+import { visualizer } from 'rollup-plugin-visualizer'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +11,12 @@ export default defineConfig({
     lib: {
       name: 'nostr-editor',
       entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es', 'umd'],
+      formats: ['es', 'umd', 'cjs'],
     },
     sourcemap: true,
     minify: false,
   },
-  plugins: [dts({ rollupTypes: true })],
+  plugins: [dts({ rollupTypes: true }), peerDepsExternal(), visualizer()],
   test: {
     globals: true,
     environment: 'happy-dom',
